649030a469aefb2ca935f6f6e6aef1c6251673fb
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-27 14:39:12 -0800

    changes AboutUs to use react classes

--- client/components/landingPageComponents/aboutUs/AboutUs.js
+++ client/components/landingPageComponents/aboutUs/AboutUs.js
@@ -3,20 +3,22 @@ import GroupMember from './subComponents/GroupMember';
 
 const corianderMembers = ['Max', 'Jota', 'Egan'];
 
-//state isnt used here (about us not going to be changing much, no need state)
-const memberList = () => {
-  const groupMembers = corianderMembers.map((member) => {
-    return <GroupMember name={member}/>
-  });
+const groupMembers = corianderMembers.map((member) => {
+  return <GroupMember name={member} />
+});
 
-  return (
-    <div>
-      <h3>Cartographic Coriander</h3>
-      <ul>
-        {groupMembers}
-      </ul>
-    </div>
-  )
-}
+const AboutUs = React.createClass({
 
-export default memberList;
+  render() {
+    return (
+      <div>
+        <ul>
+          {groupMembers}
+        </ul>
+      </div>
+    )
+  }
+
+});
+
+export default AboutUs;
